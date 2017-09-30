9c9959867510de8e98c68de9aa2e5d9f7a70bddb
Roman Melnychok <roman.melnychok@gmail.com>
2017-09-30 17:09:44 +0300

    Initial commit

---
+++ CalculatorVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Roman Melnychok on 30.09.17.
+//  Copyright © 2017 Roman Melnychok. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
